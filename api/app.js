619aea8be9ce8f8f500e39a8322f4603aeedec8c
coutinho <coutinho.andre@icloud.com>
2020-01-26 15:09:36 +0000

    fix api 404

--- api/app.js
+++ api/app.js
@@ -47,12 +47,7 @@ passport.use(new JWTStrategy({
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(passport.initialize());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -61,21 +56,5 @@ app.use('/api/', indexRouter);
 app.use('/api/user', usersRouter);
 app.use('/api/post', postRouter);
 app.use('/api/group', groupRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+	res.write('Welcome to our API');
+	res.end();
 });
 
 module.exports = router;
